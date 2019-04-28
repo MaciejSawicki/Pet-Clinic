79460261340cf74e67a61056750f4c7f1fc64cf2
MaciejSawicki <dabster07@gmail.com>
2019-04-28 20:37:47 +0200

    Added Controllers and Services Mapping

---
+++ pet-clinic-data/src/main/java/pl/sawicki/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package pl.sawicki.petclinic.services.map;
+
+import pl.sawicki.petclinic.model.Vet;
+import pl.sawicki.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+   
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+}
---
+++ pet-clinic-web/src/main/java/pl/sawicki/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package pl.sawicki.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners"})
+    public String listOwners() {
+
+        return "owners/index";
+    }
+}
