81d29b1954b848233c508f8ac41f843ecc6886ba
MaciejSawicki <dabster07@gmail.com>
2019-04-28 21:19:15 +0200

    Created Owners list table and updated Owners controller.

--- pet-clinic-web/src/main/java/pl/sawicki/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/sawicki/petclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package pl.sawicki.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sawicki.petclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
