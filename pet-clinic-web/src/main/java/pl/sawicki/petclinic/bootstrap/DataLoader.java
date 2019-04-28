bce24379590d8a31ac71de98d151cab2eb6fb208
MaciejSawicki <dabster07@gmail.com>
2019-04-28 21:37:52 +0200

    Refactored Map Implementation to abstract ID generation.

--- pet-clinic-web/src/main/java/pl/sawicki/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/sawicki/petclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFistName("Maciej");
         owner1.setLastName("Sawicki");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFistName("ExampleName");
         owner2.setLastName("ExampleSurname");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners loading finished...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFistName("Primary");
         vet1.setLastName("Vet");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFistName("Secondary");
         vet2.setLastName("Vet");
 
