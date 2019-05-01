60517b94d0ce1f86bdea76f83444817fe6ddc981
MaciejSawicki <dabster07@gmail.com>
2019-05-01 19:34:06 +0200

    added Visit and update Owner and Pet

--- pet-clinic-data/src/main/java/pl/sawicki/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/sawicki/petclinic/model/Pet.java
@@ -5,7 +5,9 @@ import java.time.LocalDate;
 public class Pet extends BaseEntity {
 
     private PetType petType;
+
     private Owner owner;
+
     private LocalDate birthDate;
 
     public PetType getPetType() {
