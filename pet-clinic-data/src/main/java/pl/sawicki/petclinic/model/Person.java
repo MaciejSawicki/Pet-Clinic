8806e7edd2386c38e69184546208ee9b3ded5df6
MaciejSawicki <dabster07@gmail.com>
2019-05-22 18:49:01 +0200

    Created JPA entities: Owner, Pet, PetType. Updated class person with JPA annotations.

--- pet-clinic-data/src/main/java/pl/sawicki/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/sawicki/petclinic/model/Person.java
@@ -1,8 +1,15 @@
 package pl.sawicki.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String fistName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFistName() {
